484f4815106fa9575110a453099886caa5577cc9
Little mind from space <726113394@qq.com>
2026-03-17 16:35:51 +0800

    Update speech.js

--- api/speech.js
+++ api/speech.js
@@ -7,6 +7,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
+  // 处理预检请求（OPTIONS）
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
